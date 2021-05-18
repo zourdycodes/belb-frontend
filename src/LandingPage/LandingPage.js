20772c86b849c23519d42811cdf8ebd79d2d5b87
muhammadzourdy <zhafarmuhammad12@gmail.com>
2021-05-18 17:10:14 +0700

    clean up: build top navbar

---
+++ src/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TopNav from "./TopNav/TopNav";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <TopNav />
+    </div>
+  );
+};
+
+export default LandingPage;
---
+++ src/LandingPage/TopNav/TopNav.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./TopNav.module.css";
+
+const TopNav = () => {
+  return (
+    <div className={styles["top-nav"]}>
+      <div className={styles.left}>
+        <span>Write a review</span>
+        <span>Events</span>
+      </div>
+      <div className={styles.right}>
+        <span>Login</span>
+        <button className="button">Sign up</button>
+      </div>
+    </div>
+  );
+};
+
+export default TopNav;
