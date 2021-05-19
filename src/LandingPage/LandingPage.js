6b23bfcc01f83a40b58a8eab843aafe4f9a0d0e3
muhammadzourdy <zhafarmuhammad12@gmail.com>
2021-05-19 08:27:00 +0700

    add: clean up search bar components

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -2,12 +2,14 @@ import React from "react";
 import TopNav from "./TopNav/TopNav";
 import logo from "../assets/logo-belb.png";
 import styles from "./LandingPage.module.css";
+import SearchBar from "../SearchBar/SearchBar";
 
 const LandingPage = () => {
   return (
     <div>
       <TopNav />
       <img src={logo} alt="logo-belb-frontend" className={styles.logo} />
+      <SearchBar />
     </div>
   );
 };
---
+++ src/SearchBar/SearchBar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "./SearchBar.module.css";
+
+const SearchBar = () => {
+  return (
+    <div className="field has-addons">
+      <p className="control">
+        <button className="button is-static is-medium">Search</button>
+      </p>
+      <p className="control">
+        <input
+          //! USING INTERPOLATION with BACKTICK
+          className={`input is-medium ${styles["input-control"]}`}
+          type="text"
+          placeholder="Search Your Spot is-medium"
+        />
+      </p>
+      <p className="control">
+        <button className="button is-static is-medium">NEAR</button>
+      </p>
+      <p className="control">
+        <input
+          //! USING INTERPOLATION with BACKTICK
+          className={`input is-medium ${styles["input-control"]}`}
+          type="text"
+          placeholder="Search Near You!"
+        />
+      </p>
+      <button className={`button is-medium ${styles["search-button"]}`}>
+        <span className="icon is-small">
+          <i className={`fas fa-search ${styles["search-icon"]}`}></i>
+        </span>
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;
